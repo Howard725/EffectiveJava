e46017bb9ecb759437f19a1275a77116bb9f0de2
Wangren725 <wangren725@163.com>
2016-06-15 13:36:40 +0800

    item 42

---
+++ src/main/java/org/effectivejava/reimplement/chapter07/item42/TestDriver.java
@@ -0,0 +1,15 @@
+package org.effectivejava.reimplement.chapter07.item42;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/6/15.
+ */
+public class TestDriver {
+
+    public static void main(String[] args){
+        int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        List<int[]> homophones = Arrays.asList(intArray);
+    }
+}
